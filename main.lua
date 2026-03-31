ca51a705eb9751fc637d422b46c3ffacc5f4f6c4
learnhtsd <160567527+learnhtsd@users.noreply.github.com>
2026-03-31 15:26:05 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local User = "learnhtsd"
 local Repo = "Minecraft"
 local Branch = "main" 
-local Version = "v0.0.002"
+local Version = "v0.0.003"
 
 -- ==========================================
 -- UI ENGINE START
